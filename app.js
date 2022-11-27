4c1fa72c0bbd9c35cfc1a8bfed2d0ed2cf6989b2
akjmalamin7 <thpi205526@gmail.com>
2022-11-27 15:37:35 +0600

    incomplete

--- app.js
+++ app.js
@@ -1,42 +1,86 @@
-
-/*validation*/
 let signInForm = document.getElementById("signInForm");
-signInForm.addEventListener("submit",function(e){
-    e.preventDefault();
-    let formData = {};
-    ;[...this.elements].forEach((el,index)=>{
-        if(el.type !== "submit"){
-            if(el.name === "email"){
-                console.log("error email")
-
-            }else{
-                console.log("error pass")
+let passwordField = document.getElementById("signInInputPassword");
+passwordField.addEventListener("input", function(e){
+  console.log(e.target.type)
+    if (e.target.type === 'password') {
+        if (e.target.value.length >= 8) {
+            document.getElementById("passwordErrorMessage").innerHTML = ""
+        } else {
+            
+            if(e.target.value.length <= 0){
+                console.log(1)
+                document.getElementById("passwordErrorMessage").innerHTML = `Password required`
+            } else if(e.target.value.length < 7){
+                document.getElementById("passwordErrorMessage").innerHTML = `Password must be 8 characters`
             }
-            formData[el.name] =  el.value;
-         
         }
-    });
-    console.log(formData)
+    }
 });
 
+signInForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let formData = {};
+    let emptyError = {};
+    ;[...this.elements].forEach((el, index) => {
+        if (el.type !== "submit") {
+            !el.value && (emptyError[el.name] = el.getAttribute("data-error"))
+            formData[el.name] = el.value;
+        }
+        el.addEventListener("input", function (e) {
+            if (el.type === 'email') {
+                if (el.value.length > 0) {
+                    document.getElementById(emptyError[el.type]).innerHTML = ""
+                } else {
+                    document.getElementById(emptyError[el.type]).innerHTML = `${el.type} required`
+                }
+            } else if (el.type === 'password') {
+                if (el.value.length >= 8) {
+                    document.getElementById(emptyError[el.type]).innerHTML = ""
+                } else {
+                    if(el.value.length <= 0){
+                        document.getElementById(emptyError[el.type]).innerHTML = `${el.type} required`
+                    } else if(el.value.length < 7){
+                        document.getElementById(emptyError[el.type]).innerHTML = `${el.type} must be 8 characters`
+                    }
+                }
+            }
+        });
+    });
+    let flag = false;
+    if (emptyError["email"]) {
+        document.getElementById(emptyError["email"]).innerHTML = "email required"
+        flag = true;
+    }
+    if (emptyError["password"]) {
+        document.getElementById(emptyError["password"]).innerHTML = "password required"
+        !flag && (flag = true)
+    }
+    if (flag) return
+    apiCall(formData)
+});
+const apiCall = (formData) => {
+    console.log(formData)
+    alert("Success");
+    clearTimeout(()=>{
+        alert("Success");
+    }, 1000)
+}
 
-
-/*is show password*/
 let eyeIconWrapper = document.getElementsByClassName("password_hide_icon");
-function isShowPassword(){
-    ;[...eyeIconWrapper].forEach((iconWrapper, index)=>{
-        iconWrapper.addEventListener("click", function(){
-           
+function isShowPassword() {
+    ;[...eyeIconWrapper].forEach((iconWrapper, index) => {
+        iconWrapper.addEventListener("click", function () {
+
             let inputElement = document.getElementById(iconWrapper.getAttribute("data-input"));
-            if(inputElement.type === "password"){
+            if (inputElement.type === "password") {
                 inputElement.type = "text";
                 iconWrapper.classList.add("show");
             }
-            else if(inputElement.type === "text"){
+            else if (inputElement.type === "text") {
                 inputElement.type = "password";
                 iconWrapper.classList.remove("show");
             }
-           
+
         });
     });
 }
